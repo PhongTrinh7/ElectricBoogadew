5efb8827fc10bd08870b321d8f93300179f77ea5
Wesleyzee <wzheng56@gmail.com>
2018-10-30 23:18:13 -0700

    Update MenuState.java

--- src/MenuState.java
+++ src/MenuState.java
@@ -6,12 +6,19 @@ public class MenuState extends State {
     }
 
     @Override
+    //NEW When you press the right and left mouse button at the same time, it will
+    //switch to gameState.
     public void tick() {
-
+        if(game.getMouseManager().isLeftPressed() && game.getMouseManager().isRightPressed()){
+            State.setState(game.gameState);
+        }
     }
 
     @Override
+    //NEW Test that makes a small red square follow your mouse.
     public void render(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(game.getMouseManager().getMouseX(), game.getMouseManager.getMouseY(), 8, 8);
 
     }
 }
