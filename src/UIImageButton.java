72c031f629352dd71af8e36805d9967f358bba8b
Wesleyzee <wzheng56@gmail.com>
2018-11-07 20:24:04 -0800

    Create UIImageButton.java

---
+++ src/UIImageButton.java
@@ -0,0 +1,32 @@
+public class UIImageButton extends UIObject{
+	
+	private ClickListener clicker;
+
+	public UIImageButton(float x, float y, int width, int height, ClickListener clicker) {
+		super(x, y, width, height);
+		this.clicker = clicker;
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		if(hovering) {
+			g.setColor(Color.GREEN);
+			g.fillRect((int) x, (int) y, width , height);
+		}
+		else {
+			g.setColor(Color.RED);
+			g.fillRect((int) x, (int) y, width, height);
+		}
+	}
+
+	@Override
+	public void onClick() {
+		clicker.onClick();
+	}
+
+}
