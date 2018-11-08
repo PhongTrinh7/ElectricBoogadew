45dd715f9483e1cc735901c7198c3c8494043999
Wesleyzee <wzheng56@gmail.com>
2018-11-07 21:15:40 -0800

    Add files via upload

--- src/CombatState.java
+++ src/CombatState.java
@@ -1,22 +1,56 @@
-import java.awt.*;
+package dev.pro.game.states;
 
-public class CombatState extends State {
+import java.awt.Graphics;
 
-    private Player skele;
+import dev.pro.game.Game;
+import dev.pro.game.gfx.Animation;
+import dev.pro.game.gfx.Assets;
+import dev.pro.game.ui.ClickListener;
+import dev.pro.game.ui.UIImageButton;
+import dev.pro.game.ui.UIManager;
 
-    public CombatState(Game game) {
-        super(game);
-        skele = new Player(game, 1000, 400, "Skeleman", Assets.skeleman);
+public class CombatState extends State{
+	
+    private UIManager combatUI;
+	private Animation skele_tele_animation;
+	int skeleX = 150;
+	
+	public CombatState(Game game) {
+		super(game);
+		
+		combatUI = new UIManager(game);
+		game.getMouseManager().setUIManager(combatUI);
+		combatUI.addObject(new UIImageButton(200, 200, 64, 64, new ClickListener() {
+			
+			public void onClick() {
+				game.getMouseManager().setUIManager(null);
+				State.setState(game.gameState);
+			}}));
+		
+		
+		skele_tele_animation = new Animation(500, Assets.skeleman_tele);
+		  
     }
 
-    @Override
-    public void tick() {
-        skele.tick();
-    }
+	@Override
+	public void tick() {
+	    combatUI.tick();
+		
+		skele_tele_animation.tick();
+		
+		if(skele_tele_animation.getIndex() == 2) {
+			skeleX = 500;
+		}
+		if(skele_tele_animation.getIndex() == 0) {
+			skeleX = 150;
+		}
 
-    @Override
-    public void render(Graphics g) {
-        g.drawImage(Assets.forestBack, 0, 0, 1280, 720, null);
-        skele.render(g);
-    }
+	}
+
+	@Override
+	public void render(Graphics g) {
+		combatUI.render(g);
+		g.drawImage(skele_tele_animation.getCurrentFrame(), skeleX, 530, 150, 100, null);
+	}
+
 }
