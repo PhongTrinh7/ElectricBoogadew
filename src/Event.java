0a09a81ef743180090d5a9db32b2015ad22290f0
Wesleyzee <wzheng56@gmail.com>
2018-12-04 18:48:05 -0800

    Add files via upload

--- src/Event.java
+++ src/Event.java
@@ -1,9 +1,14 @@
+package dev.pro.game.Events;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+import dev.pro.game.Game;
+import dev.pro.game.gfx.Assets;
+import dev.pro.game.states.CombatState;
+import dev.pro.game.states.State;
 
 public class Event {
 	
@@ -11,6 +16,7 @@ public class Event {
     private int page;
     private Game game;
     private boolean eventing;
+    private boolean switchButton = false;
 
     public Event (Game game, BufferedImage page) {
         pages = new ArrayList<>();
@@ -39,21 +45,32 @@ public class Event {
     }
     
     public void tick() {
-		if(game.getKeyManager().one) {
-		    page = 0;
-			eventing = false;
-		}
-		if(game.getKeyManager().two) {
-		    page = 1;
-            State.setState(new CombatState(game, game.skeleDog));
-		}
-		if(game.getKeyManager().three) {
-			eventing = false;
-		}
+    	if(switchButton) { //For when the choices pop up, switches the result of pressing the right arrow key.
+    		if(game.getKeyManager().right) {
+    			eventing = false;
+    		}
+    	}
+    	else {
+    		if(game.getKeyManager().right) {
+    			State.setState(new CombatState(game, game.skeleDog));
+    		}
+    	}
+    	
+    		if(game.getKeyManager().one) {
+    			page = 1;
+                switchButton = true;
+    		}
+    		if(game.getKeyManager().two) {
+    			page = 2;
+    		}
+    		if(game.getKeyManager().three) {
+    			eventing = false;
+    		}
     }
 
     public void render(Graphics g) {
         g.drawImage(pages.get(page), 160, 90, 960, 540, null);
+
     }
 
 }
